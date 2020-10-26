b451c17f3b69f1b3fe72e4ed259a11d2de6ab580
Hua Zhu <zhu.lotus@outlook.com>
2020-10-26 19:56:57 +0800

    添加数据类和Haversin计算模块

---
+++ Data.h
@@ -0,0 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-26 18:55:26
+ * @LastEditTime: 2020-10-26 19:20:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \ca_project1\Data.h
+ */
+
+// 创建保存经纬度信息的类
+// 经度：longitude; 纬度：latitude;
+
+class Data
+{
+    public:
+            
+        float longitude;
+        float latitude;
+        Data(float , float);
+        ~Data();
+        int dataShow();
+
+    private:      
+};
---
+++ mian.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+#include "Data.h"
+#include "Haversin.h"
+
+using namespace std;
+
+int main()
+{
+
+    Data data1(10.0, 11.0);
+    Data data2(12.0, 13.0);
+
+    data1.dataShow();
+
+
+}
