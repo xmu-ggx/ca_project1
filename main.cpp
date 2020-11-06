c3a330231c83347bb897fde8fb4dc18dddbf3f5e
xmu-ggx <1415606326@qq.com>
2020-11-06 14:00:37 +0800

    backup

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 int main()
 {
-    ExtU rtU;
+    ExtU rtU;   //matlab -> c 输入结构体
     ExtY rtY;
-    ExtY res;
-    Data data;
+    ExtY res;   //matlab -> c 输出结构体
+    Data data;  // asn1 -> c  数据结构体
 
     rtU.In1 = 122.446;
     rtU.In2 = 28.999;
     rtU.In3 = 122.498;
     rtU.In4 = 29.004;
 
-    res = model_new_step(rtU, rtY);
+    res = model_new_step(rtU, rtY);  // 算法实现函数
     cout << res.Out1 << endl;
     cout << res.Out2 << endl;
 
@@ -25,6 +25,8 @@ int main()
 
     cout << "data elements-------------" << endl;
     cout << data.distane << "\t" << data.angle << endl;
+
+    
 
     return 0;
 }
