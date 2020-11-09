949049eee70d4fc9a580ca8274dc16da9b5fccad
xmu-ggx <1415606326@qq.com>
2020-11-09 10:45:39 +0800

    modelfy socket

--- socket/server.cpp
+++ socket/server.cpp
@@ -1,34 +1,36 @@
-#include<WINSOCK2.H>
+#include<winsock2.h>
 #include<iostream>
 #pragma comment(lib,"WS2_32.lib")
 using namespace std;
 
 int main()
 {
-WSADATA wsaData;//初始化
+WSADATA wsaData;  //init data
 SOCKET RecvSocket;
-sockaddr_in RecvAddr;//服务器地址
-int Port=4000;//服务器监听地址
-char RecvBuf[1024];//发送数据的缓冲区
-int BufLen=1024;//缓冲区大小
+
+sockaddr_in RecvAddr;   // server aaddress
+int Port=4000;// server lisen port
+char RecvBuf[1024]; 
+int BufLen=1024;  // buffer
+
 sockaddr_in SenderAddr;
 int SenderAddrSize=sizeof(SenderAddr);
 //初始化Socket
 WSAStartup(MAKEWORD(2,2),&wsaData);
-//创建接收数据报的socket
-RecvSocket=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
-//将socket与制定端口和0.0.0.0绑定
+
+RecvSocket=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);  // build socket
+
 RecvAddr.sin_family=AF_INET;
 RecvAddr.sin_port=htons(Port);
 RecvAddr.sin_addr.s_addr=htonl(INADDR_ANY);
-bind(RecvSocket,(SOCKADDR *)&RecvAddr,sizeof(RecvAddr));
-//调用Recvfrom函数在绑定的socket上接收数据
+bind(RecvSocket, (SOCKADDR *)&RecvAddr, sizeof(RecvAddr));
+
 printf("receiving datagrams...\n");
 recvfrom(RecvSocket,RecvBuf,BufLen,0,(SOCKADDR *)&SenderAddr,&SenderAddrSize);
-//关闭socket，结束接收数据
+
 printf("finished receiving,closing socket..\n");
 closesocket(RecvSocket);
-//释放资源，退出
+
 printf("Exiting.\n");
 WSACleanup();
 return 0;
